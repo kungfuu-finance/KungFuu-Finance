e90c4df6f69851745ca70619f2965622d9115261
tyrone-frog-biggums <97226993+tyrone-frog-biggums@users.noreply.github.com>
2022-04-01 09:41:26 +0200

    V1.0

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-    <MoralisProvider appId="gKECafvmKC1HZerUI2xlpqYcDENCozuKvTJRTIlM" serverUrl="https://nbsbir88n3ng.usemoralis.com:2053/server">
+    <MoralisProvider appId="PTfVKUj7p9UnVCP09mMAmIbW9UKazO65a60CBun3" serverUrl="https://lxbxlb3irrme.usemoralis.com:2053/server">
           <App />
     </MoralisProvider>,
   document.getElementById('root')
